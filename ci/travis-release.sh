18cfa311eb2f8d57010b95c86f7add6abc73fa33
Robert Rossmann <rr.rossmann@me.com>
2017-09-05 21:53:31 +0200

    chore: set up travis releases

---
+++ ci/travis-release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Set up git user
+git config user.name "Travis-CI"
+git config user.email "travis@travis-ci.org"
+
+# Set up remote
+git remote add pushback "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+
+npx lerna outdated
+npx lerna publish \
+  --sort \
+  --conventional-commits \
+  --yes \
+  --git-remote pushback \
+  --message "chore: release [ci skip]"
