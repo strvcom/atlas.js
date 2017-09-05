047070b397d37e39dd8570cdb9540bbee2f2dd29
Robert Rossmann <rr.rossmann@me.com>
2017-09-05 23:42:44 +0200

    chore(build): Checkout the release branch before release

--- ci/travis-release.sh
+++ ci/travis-release.sh
@@ -6,6 +6,7 @@ git config user.email "travis@travis-ci.org"
 
 # Set up remote
 git remote add pushback "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+git checkout "${TRAVIS_BRANCH}"
 
 npx lerna outdated
 npx lerna publish \
