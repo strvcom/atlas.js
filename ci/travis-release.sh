cee2b0c3d2f79f5abd24943c2522dda23131ea7f
Robert Rossmann <rr.rossmann@me.com>
2017-09-06 00:58:54 +0200

    chore: Enhance release script, fix npm publishing

--- ci/travis-release.sh
+++ ci/travis-release.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
-# Set up git user
-git config user.name "Travis-CI"
-git config user.email "travis@travis-ci.org"
+# Set up git user based on last commit's author
+git config user.name "$(git show --quiet --format=%an "${TRAVIS_COMMIT}")"
+git config user.email "$(git show --quiet --format=%ae "${TRAVIS_COMMIT}")"
 
-# Set up remote
+# Authorise npm for publishing
+cat <<NPMRC >> .npmrc
+@atlas.js:registry=https://registry.npmjs.org/
+//registry.npmjs.org/:_authToken=${NPM_TOKEN}
+access=public
+NPMRC
+echo packages/* | xargs -n 1 cp .npmrc
+
+# Set up git remote
 git remote add pushback "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 git checkout "${TRAVIS_BRANCH}"
 
+# Publish!
 npx lerna outdated
 npx lerna publish \
   --sort \
