3612c284fe4c78724a5e89e5e450fbc1d458c553
Robert Rossmann <rr.rossmann@me.com>
2017-11-24 14:47:42 +0100

    feat(objection): Expose ref, lit functions

--- packages/objection/src/index.mjs
+++ packages/objection/src/index.mjs
@@ -2,19 +2,23 @@ import Service from './service'
 import MigrationAction from './migration'
 import {
   compose,
+  lit,
   mixin,
   Model,
   NotFoundError,
+  ref,
   transaction,
   ValidationError,
 } from 'objection'
 
 export {
   compose,
+  lit,
   MigrationAction,
   mixin,
   Model,
   NotFoundError,
+  ref,
   Service,
   transaction,
   ValidationError,
