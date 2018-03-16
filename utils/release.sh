174925ba31bffa39d556d5fad5dfd4b78c2bfe95
Robert Rossmann <rr.rossmann@me.com>
2018-03-16 11:11:16 +0100

    chore: Add publishConfig to all package manifests

--- utils/release.sh
+++ utils/release.sh
@@ -5,12 +5,10 @@ git config user.name "$(git show --quiet --format=%an "${TRAVIS_COMMIT}")"
 git config user.email "$(git show --quiet --format=%ae "${TRAVIS_COMMIT}")"
 
 # Authorise npm for publishing
-cat <<NPMRC >> .npmrc
+cat <<NPMRC >> ~/.npmrc
 @atlas.js:registry=https://registry.npmjs.org/
 //registry.npmjs.org/:_authToken=${NPM_TOKEN}
-access=public
 NPMRC
-echo packages/* | xargs -t -n 1 cp .npmrc
 
 # Set up git remote
 git remote add pushable-origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
