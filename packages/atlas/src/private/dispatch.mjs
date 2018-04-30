3eff825310c76f46eb4d5384d1a43664d338d6cb
Robert Rossmann <rr.rossmann@me.com>
2018-04-30 14:01:07 +0200

    chore(atlas): The internal ::dispatch() now only accepts single event

--- packages/atlas/src/private/dispatch.mjs
+++ packages/atlas/src/private/dispatch.mjs
@@ -4,26 +4,20 @@
  * This function takes variable number of events to be dispatched to hooks
  *
  * @private
- * @param     {String|Array}    events    The events' names
- * @param     {mixed}           subject   The thing that is related to the event (ie. a component)
- *                                        It is given to the event handler on input.
+ * @param     {String}    event     The event's name
+ * @param     {mixed}     subject   The thing that is related to the event (ie. a component)
+ *                                  It is given to the event handler on input.
  * @this      Map
  * @return    {Promise<void>}
  */
-async function dispatch(events, subject) {
-  events = Array.isArray(events)
-    ? events
-    : [events]
-
+async function dispatch(event, subject) {
   const tasks = []
 
   for (const [, hook] of this) {
-    for (const event of events) {
-      // Is this hook listening for the event being dispatched?
-      if (typeof hook.component[event] === 'function') {
-        hook.component.log.debug({ event }, 'hook:event')
-        tasks.push(hook.component[event](subject))
-      }
+    // Is this hook listening for the event being dispatched?
+    if (typeof hook.component[event] === 'function') {
+      hook.component.log.debug({ event }, 'hook:event')
+      tasks.push(Promise.resolve(hook.component[event](subject)))
     }
   }
 
