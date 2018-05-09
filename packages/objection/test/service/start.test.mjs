97b38d4ffa6d8329afb3c5a4a206920b0eb97fd6
Robert Rossmann <rr.rossmann@me.com>
2018-05-09 12:34:15 +0200

    chore(objection): Prepare test config separately

--- packages/objection/test/service/start.test.mjs
+++ packages/objection/test/service/start.test.mjs
@@ -4,12 +4,13 @@ import { Service as Objection } from '../..'
 describe('Objection::start()', () => {
   let service
   let instance
+  let opts
 
 
   beforeEach(async function() {
-    service = new Objection({
+    opts = {
       atlas: {
-        require: sinon.stub(),
+        require: sinon.stub().returns(models),
       },
       log: {},
       config: {
@@ -19,8 +20,8 @@ describe('Objection::start()', () => {
         },
         models: path.resolve(__dirname, './models'),
       },
-    })
-
+    }
+    service = new Objection(opts)
     instance = await service.prepare()
     this.sandbox.stub(instance.connection, 'raw').resolves()
 
