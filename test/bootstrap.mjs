a24890eb803d76cdb73155ebb090281b3dcfb412
Robert Rossmann <rr.rossmann@me.com>
2018-05-10 13:24:23 +0200

    chore: Switch back to sinon-chai

--- test/bootstrap.mjs
+++ test/bootstrap.mjs
@@ -1,14 +1,14 @@
 import sinon from 'sinon'
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
-import chaiSinon from 'chai-sinon'
+import sinonChai from 'sinon-chai'
 
 // Make sure our tests always run in the 'test' environment
 // eslint-disable-next-line no-process-env
 process.env.NODE_ENV = 'test'
 
 chai.use(chaiAsPromised)
-chai.use(chaiSinon)
+chai.use(sinonChai)
 
 // Make our lives easier - expose these packages as globals since they are used everywhere in tests
 global.expect = chai.expect
