e9fac4d47612af86615d449dddd207f8637f3fdd
Robert Rossmann <rr.rossmann@me.com>
2018-05-11 15:04:03 +0200

    fix(errors): preserve FrameworkError's name in thrown errors

--- packages/errors/src/index.mjs
+++ packages/errors/src/index.mjs
@@ -1,7 +1,15 @@
 /**
  * Generic error class for all errors explicitly created within @atlas.js
+ * @extends Error
  */
-class FrameworkError extends Error {}
+class FrameworkError extends Error {
+  constructor(message) {
+    super(message)
+    // Fix error name in stack traces
+    Error.captureStackTrace(this, this.constructor)
+    this.name = this.constructor.name
+  }
+}
 
 export {
   FrameworkError,
