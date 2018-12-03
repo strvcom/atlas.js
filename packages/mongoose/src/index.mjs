1b5cac59394841360c86478af40755b5f5e44cf1
Robert Rossmann <rr.rossmann@me.com>
2018-12-03 15:53:34 +0100

    feat(mongoose): export the whole mongoose package
    Useful when doing custom stuff and you need access to mongoose internals. Also great for type hinting to some extent.

--- packages/mongoose/src/index.mjs
+++ packages/mongoose/src/index.mjs
@@ -1,13 +1,14 @@
-import {
-  Schema,
-  SchemaTypes,
-} from 'mongoose'
+import mongoose from 'mongoose'
 import Service from './service'
 import ModelsHook from './models'
+
+const Schema = mongoose.Schema
+const SchemaTypes = mongoose.SchemaTypes
 
 export {
   Service,
   ModelsHook,
   Schema,
   SchemaTypes,
+  mongoose,
 }
