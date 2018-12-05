c5e94207f08476dd5c2914b57782d63b14bc3093
Robert Rossmann <rr.rossmann@me.com>
2018-12-05 16:44:42 +0100

    feat(nodemailer): add typings

---
+++ packages/nodemailer/src/index.d.ts
@@ -0,0 +1,56 @@
+declare module '@atlas.js/nodemailer' {
+  import * as nodemailer from 'nodemailer'
+  import AtlasService from '@atlas.js/service'
+
+  /**
+   * Send emails using nodemailer from within Atlas
+   */
+  class Service extends AtlasService {
+    /** Service runtime configuration values */
+    config: Service.Config
+
+    prepare(): Promise<nodemailer.Transporter>
+    start(service: nodemailer.Transporter): Promise<nodemailer.Transporter>
+    stop(service: nodemailer.Transporter): Promise<void>
+  }
+
+  namespace Service {
+    /** Configuration schema available to this service */
+    interface Config {
+      /**
+       * The transport to be used for sending emails
+       *
+       * Either provide the transport module itself or specify it as a string and it will be
+       * `require()`d.
+       */
+      transport: Function | string
+      /**
+       * Transport-specific options
+       *
+       * Check the docs for the transporter you are going to use for available options.
+       */
+      options: object
+
+      plugins?: Array<PluginConfiguration>
+    }
+
+    interface PluginConfiguration {
+      /**
+       * The plugin to be added to the transporter
+       *
+       * Either provide the plugin module itself or specify it as a string and it will be
+       * `require()`d.
+       */
+      plugin: Function | string
+      /** Event to which this plugin should be attached */
+      event: 'compile' | 'stream'
+      /** Plugin configuration options */
+      options: object
+    }
+  }
+
+  export {
+    Service,
+    nodemailer,
+  }
+}
