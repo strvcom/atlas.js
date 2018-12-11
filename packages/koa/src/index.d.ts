8392aaa5e0d4dd7d51caa0d241a2e08a2ce4bd9a
Robert Rossmann <rr.rossmann@me.com>
2018-12-11 11:29:48 +0100

    fix(koa): add `server` to the exported Koa instance type definition

--- packages/koa/src/index.d.ts
+++ packages/koa/src/index.d.ts
@@ -1,9 +1,20 @@
+import * as http from 'http'
+
 import Server from './server'
 import ContextHook from './context'
 import WebsocketHook from './websocket'
-import * as Koa from 'koa'
+import * as koa from 'koa'
 
 declare module '@atlas.js/koa' {
+  class Koa extends koa {
+    /**
+     * The underlying HTTP server created for this Koa instance
+     *
+     * The server is only created during the `start` Atlas lifecycle and is destroyed during `stop`.
+     */
+    server: http.Server
+  }
+
   export {
     Server,
     ContextHook,
