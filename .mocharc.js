c972fcdc17716ed81ef2c2c90490cefa917e1154
Robert Rossmann <rr.rossmann@me.com>
2019-03-06 11:21:09 +0100

    chore: add some user-specific Mocha reporter configurations

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,12 @@
 'use strict'
 
 module.exports = {
+  // Options for mocha-reporter-remote
+  // Other reporters will ignore these options.
+  reporterOption: [
+    `root=${__dirname}`,
+    'nostats=1',
+  ],
   colors: true,
   checkLeaks: true,
   require: [
