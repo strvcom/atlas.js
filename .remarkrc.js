b7e89ad37c5991a9f6eb27d21350eaa75e97a7a2
Robert Rossmann <rr.rossmann@me.com>
2019-08-13 18:09:09 +0200

    chore: migrate remark to @strv/remark-config 🎉

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,18 +1,3 @@
 'use strict'
 
-module.exports = {
-  settings: {
-    emphasis: '_',
-  },
-  plugins: [
-    'preset-lint-recommended',
-    'preset-lint-consistent',
-    'preset-lint-markdown-style-guide',
-
-    ['lint-list-item-indent', 'space'],
-    ['lint-maximum-line-length', Infinity],
-    ['lint-emphasis-marker', '_'],
-    ['lint-table-pipe-alignment', false],
-    ['lint-table-cell-padding', 'consistent'],
-  ],
-}
+module.exports = require('@strv/remark-config')
